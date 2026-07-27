26febbed7517c07ae87c6db4b5b353d845403b69
Ink <pandainvasionca@gmail.com>
2026-07-26 22:50:57 -0400

    feat(ast): start of AST infra

---
+++ include/willow/ast/pretty_print.h
@@ -0,0 +1,8 @@
+#ifndef WILLOW__AST_PRETTY_PRINT_H__
+#define WILLOW__AST_PRETTY_PRINT_H__
+
+#include <willow/ast/expression_types.h>
+
+extern void wil_ast_pretty_print(wil_ast_expr_t expr);
+
+#endif /* WILLOW__AST_PRETTY_PRINT_H__ */
